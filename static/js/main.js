9d2e5a743e53756332c4284116299d58ad209af8
zhushanhuan <18815423036@163.com>
2021-05-15 15:00:23 +0800

    详情页开发完成

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,10 @@
 $('#menutoggle').click(function(){
     $('.m-item').toggleClass('m-mobile-hide')
 });
+
+// 二维码
+$('#paybutton').popup({
+    popup : $('#pay.popup'),
+    on : 'click',
+    position : 'bottom center'
+});
